a7f76cda024f6c1e3359e556c28edf6762ca9815
Andrés Felipe Gutiérrez Rivera <felipe.gutierrezri@gmail.com>
2025-04-07 16:32:49 -0500

    Inicio de control de versiones

---
+++ Ejercicio_1.py
@@ -0,0 +1,13 @@
+# Mi primer programa en Python
+# Este programa imprime "Hola Mundo" en la consola
+
+print("Hola Mundo")
+
+print("¿Como te llamas?")
+nombre = input("Escribe tu nombre: ")
+
+print("Cuantos años tienes?")
+edad = input("Escribe tu edad: ")
+
+print("Hola " + nombre + ", tienes " + edad + " años.")
+
